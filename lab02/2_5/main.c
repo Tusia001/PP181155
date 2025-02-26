dcd691cc0cd78417b7e2e4940a01fff4500d1b85
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-02-26 14:34:03 +0100

    lab02

---
+++ lab02/2_5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int a = 3, b = 10;
+    int wynik = (int) pow(a + b, 2.0);
+    printf("%d", wynik);
+    return 0;
+}
