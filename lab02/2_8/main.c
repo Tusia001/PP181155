ae8eb282d2927b560bf2622462a8430e5c1db7aa
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-01 11:44:02 +0100

    lab02
    Zaprojektuj program, który oblicza kwadrat różnicy dwóch podanych liczb całkowitych.

--- lab02/2_8/main.c
+++ lab02/2_8/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int a = 10, b = 5;
+    int wynik = (int) pow((a-b), 2.0);
+    printf("%d", wynik);
     return 0;
 }
