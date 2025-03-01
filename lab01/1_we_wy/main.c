3ea55a4e6d3fd94549e9de8570203407700c96b8
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-01 11:16:55 +0100

    lab01

--- lab01/1_we_wy/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-    int liczba;
-    printf("Podaj liczbe calkowita: ");
-    scanf("%d", &liczba);
-    printf("Wartosc bezwzgledna: %d\n", abs(liczba));
-    return 0;
-}
