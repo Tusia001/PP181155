79f718c087ce90bfd391377ace4d3dcbc8d3c4c2
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-01 15:45:57 +0100

    lab02

---
+++ lab02/2_11/main.c
@@ -0,0 +1,12 @@
+/*Znajdü úredniπ arytmetycznπ trzech liczb zmiennoprzecinkowych. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a = 12.3, b = 10, c = 6.0009;
+    float wynik = (a+b+c)/3.0;
+    printf("Srednia arytmetyczna: %f", wynik);
+    return 0;
+}
