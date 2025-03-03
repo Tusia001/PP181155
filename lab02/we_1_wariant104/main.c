1d28db220a23bc8df7977daad0be986ba2217e48
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-03 13:54:21 +0100

    lab03

---
+++ lab02/we_1_wariant104/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, c;
+
+    printf("Podaj liczbe a: ");
+    scanf("%f", &a);
+
+    printf("Podaj liczbe b: ");
+    scanf("%f", &b);
+
+    printf("Podaj liczbe c: ");
+    scanf("%f", &c);
+
+    float wynik = c - ((b - 2*a) / (4 + a));
+
+    printf("Wynik =  %f", wynik);
+    return 0;
+}
