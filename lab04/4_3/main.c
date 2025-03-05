6307c66567e37d633910ec05840211ae6b2a11d4
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-05 14:19:03 +0100

    lab04

--- lab04/4_3/main.c
+++ lab04/4_3/main.c
@@ -1,8 +1,23 @@
+/*Wyznacz wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
+pozycje numerujemy od tylu
+11 = 1011
+pozycje      | 3 | 2 | 1 | 0|
+wartosc bitu | 1 | 0 | 1 | 1|
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int liczba = 11, pozycja1 = 3, pozycja2 = 2;
+
+    int bit = (liczba >> pozycja1) & 1;
+    printf("%d\n", bit);
+
+    int bit2 = (liczba >> pozycja2) & 1;
+    printf("%d\n", bit2);
+
     return 0;
 }
