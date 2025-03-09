711f157281fe5800e0960b46c51f09cce39e0e93
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-09 12:33:27 +0100

    lab04

---
+++ lab04/4_8/main.c
@@ -0,0 +1,14 @@
+/*Wyznacz “XOR” dwóch liczb ca³kowitych. */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 11, b = 9;
+
+    int wynik = a ^ b;
+
+    printf("%d", wynik);
+
+    return 0;
+}
