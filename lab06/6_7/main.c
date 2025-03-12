8be5b8a77d169a45ac499274fb08f6d9a3103f38
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-12 14:26:19 +0100

    lab06

---
+++ lab06/6_7/main.c
@@ -0,0 +1,20 @@
+/*Napisz program, który prosi u¿ytkownika o wprowadzenie dodatniej
+liczby ca³kowitej n, a nastêpnie oblicza i wyœwietla
+(sufit pierwiastka kwadratowego). */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int liczba, i = 0;
+
+    printf("Podaj dodatnia liczbe calkowita: ");
+    scanf("%d", &liczba);
+
+    while (i*i < liczba) {
+        i++;
+    }
+
+    printf("ceil(sqrt(%d)) = %d\n", liczba, i);
+    return 0;
+}
