a6364b481b70235da463fe2220ead18abb53b744
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-17 14:39:50 +0100

    lab07

--- lab07/6_14/main.c
+++ lab07/6_14/main.c
@@ -1,8 +1,26 @@
+/*Napisz program, który sprawdza, czy dana liczba n jest liczb¹ pierwsz¹. Program powinien sprawdziæ wszystkie potencjalne dzielniki od 2 do pierwiastka z n (wykorzystaj pêtlê do obliczenia pierwiastka). Wypisz odpowiedni komunikat. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    scanf("%d", &n);
+    if (n <2){
+        printf("Liczba nie jest ani pierwsza ani zlozona");
+        exit(-1);
+    }
+    int j;
+    while(j*j <=n){
+        j++;
+    }
+    for(int i=2;i<=j-1;i++){
+        if (n%i ==0){
+            printf("Liczba jest zlozona");
+            exit(0);
+        }
+    }
+    printf("Liczba jest pierwsza");
     return 0;
 }
