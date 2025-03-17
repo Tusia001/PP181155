f5269c8146ddf68273ebab4c1e2c51e4b3ed5654
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-17 08:40:57 +0100

    lab03

--- lab03/3_13/main.c
+++ lab03/3_13/main.c
@@ -1,8 +1,16 @@
+/*Napisz program, który przyjmuje rok jako wejœcie od u¿ytkownika i za pomoc¹ operatora
+warunkowego sprawdza, czy jest to rok przestêpny. Program powinien wyœwietlaæ “Rok
+przestêpny” lub “Rok nieprzestêpny” w zale¿noœci od wyniku.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int rok;
+    scanf("%d", &rok);
+
+     printf("%s", ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0)) ? "Rok przestepny" : "Rok nieprzestepny");
+
     return 0;
 }
