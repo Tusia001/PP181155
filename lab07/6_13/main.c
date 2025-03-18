9f210a338e38cc40ff9bda1be7d130ab48925d97
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-18 20:46:46 +0100

    lab07

--- lab07/6_13/main.c
+++ lab07/6_13/main.c
@@ -1,10 +1,19 @@
-/* SKONCZYC Napisz program realizuj¹cy algorytm Euklidesa w wersji iteracyjnej z dzieleniem. Program powinien wczytaæ dwie liczby naturalne a i b, a nastêpnie wypisaæ ich najwiêkszy wspólny dzielnik. */
+/*Napisz program realizuj¹cy algorytm Euklidesa w wersji iteracyjnej z dzieleniem. Program powinien wczytaæ dwie liczby naturalne a i b, a nastêpnie wypisaæ ich najwiêkszy wspólny dzielnik. */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d\n", a);
+
     return 0;
 }
