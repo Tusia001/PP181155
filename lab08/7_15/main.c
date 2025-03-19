15c4618dd01e788ffa1ef964203414e8395e7e20
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-19 14:44:10 +0100

    lab08

--- lab08/7_15/main.c
+++ lab08/7_15/main.c
@@ -1,4 +1,5 @@
-/* Napisz funkcję calculate15, której argumentem jest dodatnia liczba całkowita 𝑛. Funkcja ma zwracać wartość wyrażoną wzorem 𝑓(𝑛) = 𝑓(𝑛 − 1) + 2𝑓(𝑛 − 2), gdzie 𝑓(1) = 1 i 𝑓(2) = 2. Stwórz przypadek testowy. */
+/* DOKONCZYC
+Napisz funkcję calculate15, której argumentem jest dodatnia liczba całkowita 𝑛. Funkcja ma zwracać wartość wyrażoną wzorem 𝑓(𝑛) = 𝑓(𝑛 − 1) + 2𝑓(𝑛 − 2), gdzie 𝑓(1) = 1 i 𝑓(2) = 2. Stwórz przypadek testowy. */
 
 #include <stdio.h>
 #include <stdlib.h>
