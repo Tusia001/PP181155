d32498e8dd05d54380e168490a9b00e2395d53ca
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-26 13:40:00 +0100

    lab10

---
+++ lab10/8_2/main.c
@@ -0,0 +1,28 @@
+/* 2. W jednym pliku wykonaj czynnoœci:
+• Napisz funkcjê sum z dwoma argumentami typu int. Funkcja ma zwracaæ sumê wartoœci
+przekazanych przez argumenty. Stwórz przypadek testowy.
+• Napisz funkcjê sumVals z dwoma argumentami, które s¹ wskaŸnikami do zmiennych typu
+int. Funkcja powinna zwracaæ sumê wartoœci, na które wskazuj¹ te argumenty. Utwórz
+przypadek testowy.
+• Napisz funkcjê addPtr z trzema argumentami, które s¹ wskaŸnikami do zmiennych typu
+int. Funkcja ma byæ procedur¹ (typ zwracany void). Funkcja ma ustawiæ wartoœæ
+zmiennej wskazywanej przez trzeci argument funkcji jako sumê wartoœci wskazywanych
+przez dwa wczeœniejsze argumenty. Utwórz przypadek testowy. */
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int x, int y){
+    return x+y;
+}
+
+int sumVals(int *p1, int *p2){
+    return *p1 + *p2;
+}
+
+int main()
+{
+    int x = 2, y = 4;
+    printf("%d\n", sum(x,y));
+    printf("%d\n", sumVals(&x, &y));
+    return 0;
+}
