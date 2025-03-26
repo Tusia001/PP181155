6ba783f2442bf93ea78a6cfac010c8ea4df76fd3
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-26 14:34:28 +0100

    +

--- lab10/8_5/main.c
+++ lab10/8_5/main.c
@@ -3,20 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * initInits(){
-    int *ptr = malloc(3*sizeof(int));
-    if(ptr == NULL){
+int * initInts(){
+    int * ptr = malloc(3*sizeof(int));
+    if (ptr == NULL){
         return NULL;
     }
     *ptr = 5;
-    *(ptr + 1) = -12;
-    *(ptr + 2) = 33;
-    return ptr + 1;
+    *(ptr+1) = -12;
+    *(ptr+2) = 33;
+    return ptr+1;
 }
 
 int main()
 {
-    int *result = initInits();
+    int * result = initInts();
+    printf("%d\n", *(result-1));
     printf("%d\n", *result);
+    printf("%d\n", *(result+1));
+    free(result-1);
     return 0;
 }
