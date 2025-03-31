b5cb3e96ad7d8ed3e2f1b98f0e425686c05e9357
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-31 14:20:56 +0200

    lab11

--- lab11/8_12/main.c
+++ lab11/8_12/main.c
@@ -1,1 +1,23 @@
+/*Napisz funkcjê linFuncVal z trzema argumentami: wskaŸnikiem a na sta³¹ wartoœæ typu float, sta³ym wskaŸnikiem b na zmienn¹ typu float, i wskaŸnikiem x na sta³¹ wartoœæ typu float. Funkcja ma obliczaæ i zwracaæ wartoœæ funkcji liniowej y=ax+b dla argumentu x, gdzie a i b s¹ wskazywane przez odpowiednie wskaŸniki. Utwórz przypadek testowy. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float linFuncVal(const float *a, float *const b, const float *x) {
+    float wynik = (*a) * (*x) + (*b);
+    return wynik;
+}
+
+int main() {
+    float a = 2.0, b = 3.0, x = 4.0;
+
+    float y = linFuncVal(&a, &b, &x);
+
+    printf("y = %.2f * %.2f + %.2f = %.2f\n", a, x, b, y);
+
+    return 0;
+}
+
+
+
 
---
+++ lab11/8_16/main.c
@@ -0,0 +1,10 @@
+/* LABY - Napisz funkcjÍ swap z dwoma argumentami: wskaünikiem ptr1 na zmiennπ typu int i wskaünikiem ptr2 na innπ zmiennπ tego samego typu. Funkcja zamienia miejscami wartoúci wskazywane przez wskaüniki. UtwÛrz przypadek testowy. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
