405e8f3e4892493395fd3f1e9cb59b19dc5eff89
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-31 13:50:04 +0200

    lab10 poprawione

--- lab10/8_9/main.c
+++ lab10/8_9/main.c
@@ -1,16 +1,19 @@
-/* Napisz funkcjê sqrCopy z dwoma argumentami: wskaŸnikiem na sta³¹ typu int i wskaŸnikiem na zmienn¹ typu int. Funkcja ma przepisaæ kwadrat wartoœci sta³ej do zmiennej
+/* LAB - POPRAWIONE - Napisz funkcjê sqrCopy z dwoma argumentami: wskaŸnikiem na sta³¹ typu int i wskaŸnikiem na zmienn¹ typu int. Funkcja ma przepisaæ kwadrat wartoœci sta³ej do zmiennej
 wskazywanej przez drugi argument. Utwórz przypadek testowy */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void sqrCopy(int * n, int *w){
-    *n = (*w)*(*w);
+void sqrCopy(int const * p1, int *p2){
+    *p2 = (*p1) * (*p1);
 }
 
 
 int main()
 {
-    printf("Hello world!\n");
+    int x = 4, y = -3;
+    printf("%d %d\n", x, y);
+    sqrCopy(&x, &y);
+    printf("%d %d\n", x, y);
     return 0;
 }
