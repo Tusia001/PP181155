b568ff515d9e3da3e98ce57a3269c10eb2fc3516
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-31 14:40:45 +0200

    lab11

---
+++ lab11/8_17/main.c
@@ -0,0 +1,11 @@
+/* laby  - Napisz funkcjê swapSign z dwoma argumentami: wskaŸnikiem ptr1 na zmienn¹ typu double i wskaŸnikiem ptr2 na inn¹ zmienn¹ tego samego typu. Funkcja zamienia miejscami wartoœci wskazywane przez wskaŸniki, jeœli maj¹ one ró¿ne znaki. W przeciwnym razie nie robi nic. Utwórz przypadek testowy. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
