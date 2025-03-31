b5abfb5aa38a5a2af7d930629406bd2fb74056c8
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-03-31 14:32:24 +0200

    skonczyc

---
+++ lab11/8_15/main.c
@@ -0,0 +1,13 @@
+/* Napisz funkcjê absVal z jednym argumentem, którym jest wskaŸnik na zmienn¹ typu int. Funkcja oblicza wartoœæ bezwzglêdn¹ zmiennej wskazywanej przez wskaŸnik i aktualizuje tê zmienn¹. Utwórz przypadek testowy w main, aby wyœwietliæ wartoœæ zaktualizowanej zmiennej. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
