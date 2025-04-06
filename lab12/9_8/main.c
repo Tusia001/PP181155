a1e04fe9e433f624bc202d36dae3f357f69a40d9
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-04-06 17:48:51 +0200

    do poprawy

---
+++ lab12/9_8/main.c
@@ -0,0 +1,15 @@
+
+/*Stwórz funkcjê modifyValue, która przyjmuje argument typu int oraz wskaŸnik na funkcjê modifier nie przyjmuj¹c¹ ¿adnych argumentów, ale zwracaj¹c¹ wartoœæ typu int. Funkcja modifyValue powinna dodaæ do przekazanej wartoœci wynik wywo³ania funkcji modifier i zwróciæ otrzyman¹ sumê. Stwórz przypadek testowy. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+modifyValue(int arg, *modifier(int)){
+
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
