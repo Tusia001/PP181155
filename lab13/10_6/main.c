d34583c21468b5314082c393f5d64bc000b766cf
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-04-07 14:37:57 +0200

    lab13

---
+++ lab13/10_6/main.c
@@ -0,0 +1,15 @@
+/* Napisz funkcjê reverseArr, której argumentami s¹ dodatnia liczba ca³kowita n reprezentuj¹ca rozmiar tablicy oraz n-elementowa tablica tab o elementach typu int. Funkcja
+ta ma odwróciæ kolejnoœæ elementów w tablicy tab bezpoœrednio w niej (w miejscu), nie
+u¿ywaj¹c dodatkowej tablicy do przechowywania wyników. Stwórz przypadek testowy.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverseArr(int n, int tab[]){
+
+}
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
