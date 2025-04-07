0fec108b825a2801262b0727c79041995589a0fc
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-04-07 13:33:54 +0200

    we

---
+++ lab12/we_6_wariant13p/main.c
@@ -0,0 +1,11 @@
+/* ZROBIC we wariant 13P 07-04-2025, napisz funkcje multiplyByTwo z dwoma argumentami: wskaŸnikiem na sta³¹ typu float i wskaŸnikiem na zmienn¹ typu float. Funkcja ma pomno¿yæ wartoœæ sta³ej przez 2 i zapisaæ wynik pod agresem wskazanym przez drugi argument. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
