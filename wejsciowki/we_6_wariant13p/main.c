d7eface3a81e064ff72d42d50ac41af914a52a78
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-04-08 12:28:43 +0200

    we

--- wejsciowki/we_6_wariant13p/main.c
+++ wejsciowki/we_6_wariant13p/main.c
@@ -1,4 +1,4 @@
-/* ZROBIC we wariant 13P 07-04-2025, napisz funkcje multiplyByTwo z dwoma argumentami: wskaŸnikiem na sta³¹ typu float i wskaŸnikiem na zmienn¹ typu float. Funkcja ma pomno¿yæ wartoœæ sta³ej przez 2 i zapisaæ wynik pod agresem wskazanym przez drugi argument. */
+/* we wariant 13P 07-04-2025, napisz funkcje multiplyByTwo z dwoma argumentami: wskaŸnikiem na sta³¹ typu float i wskaŸnikiem na zmienn¹ typu float. Funkcja ma pomno¿yæ wartoœæ sta³ej przez 2 i zapisaæ wynik pod agresem wskazanym przez drugi argument. */
 
 
 #include <stdio.h>
