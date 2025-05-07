567e708e0e2dd7577ab6ec389b7c8ca694a2dc75
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-07 14:11:30 +0200

    lab

--- lab19/12_4/main.c
+++ lab19/12_4/main.c
@@ -1,38 +1,35 @@
 /*Napisz funkcjê minColIdx, która dostaje jako argumenty dwie dodatnie liczby ca³kowite n i m oraz dwuwymiarow¹ tablicê tablic o elementach typu int o wymiarach n x m. Funkcja ma zwróciæ indeks kolumny, w którym znajduje siê najmniejszy element (w przypadku kilku mo¿liwych indeksów, zwróæ najmniejszy z mo¿liwych). Stwórz przypadek testowy dla funkcji. */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int minColIdx(int n, int m, int **tab) {
-    int min = tab[0][0];
-    int col = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (tab[i][j] < min) {
-                min = tab[i][j];
-                col = j;
+int minColIdx(int n, int m, int **tab){
+    int minimum = tab[0][0];
+    int columnIndex = 0;
+    for(int j=0;j<m;j++){
+        for(int i=0;i<n;i++){
+            if (minimum > tab[i][j]){
+                minimum = tab[i][j];
+                columnIndex = j;
             }
         }
     }
-
-    return col;
+    return columnIndex;
 }
 
-int main() {
-    int **tab = malloc(2 * sizeof(int *));
-    tab[0] = malloc(3 * sizeof(int));
-    tab[1] = malloc(3 * sizeof(int));
-
-    tab[0][0] = 4;  tab[0][1] = -2; tab[0][2] = 9;
-    tab[1][0] = 8;  tab[1][1] = -7; tab[1][2] = 5;
-
-    int wynik = minColIdx(2, 3, tab);
-    printf("Indeks kol z min elementem: %d\n", wynik);
-
-    free(tab[0]);
-    free(tab[1]);
-    free(tab);
+int main()
+{
+    int ** tab=malloc(4*sizeof(int*));
+    tab[0] = malloc(3*sizeof(int));
+    tab[1] = malloc(3*sizeof(int));
+    tab[2] = malloc(3*sizeof(int));
+    tab[3] = malloc(3*sizeof(int));
+    tab[0][0] =  1; tab[0][1] =  2; tab[0][2] = -3;
+    tab[1][0] =  4; tab[1][1] =  5; tab[1][2] =  6;
+    tab[2][0] = -2; tab[2][1] =  1; tab[2][2] =  5;
+    tab[3][0] =  4; tab[3][1] = -3; tab[3][2] =  7;
+    printf("%d\n", minColIdx(4,3, tab));
     return 0;
 }
 
---
+++ lab19/12_6/main.c
@@ -0,0 +1,10 @@
+/*Napisz funkcjê copyArr2D, która dostaje jako argumenty dwie dodatnie liczby ca³kowite n i m oraz dwie dwumiarowe tablice tablic o elementach typu int o wymiarach n x m. Funkcja ma przepisaæ zawartoœæ drugiej tablicy do pierwszej tablicy. Stwórz przypadek testowy dla funkcji.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
