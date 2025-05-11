e9e4e81e3071c03307711493360e46f03f41149d
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-11 21:41:05 +0200

    lab

--- lab19/12_11/main.c
+++ lab19/12_11/main.c
@@ -3,8 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void revCols(int n, int m, int **tab) {
+    for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n / 2; i++) {
+            int temp = tab[i][j];
+            tab[i][j] = tab[n - 1 - i][j];
+            tab[n - 1 - i][j] = temp;
+        }
+    }
+}
+
+void printTab(int n, int m, int **tab) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", tab[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----\n");
+}
+
+int main() {
+    int n = 3, m = 4;
+    int **tab = malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        tab[i] = malloc(m * sizeof(int));
+    }
+
+    int licznik = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            tab[i][j] = licznik++;
+        }
+    }
+
+    revCols(n, m, tab);
+    printTab(n, m, tab);
+
+    for (int i = 0; i < n; i++) {
+        free(tab[i]);
+    }
+    free(tab);
+
     return 0;
 }
