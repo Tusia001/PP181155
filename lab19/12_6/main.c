b8faf97c193677f4a2e592e263ae072e346ad198
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-11 21:36:26 +0200

    lab

--- lab19/12_6/main.c
+++ lab19/12_6/main.c
@@ -3,8 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void copyArr2D(int n, int m, int **tab1, int **tab2) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            tab1[i][j] = tab2[i][j];
+        }
+    }
+}
+
+void printTable(int n, int m, int **tab) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", tab[i][j]);
+        }
+        printf("\n");
+    }
+    printf("---\n");
+}
+
+int main() {
+    int n = 2, m = 3;
+
+    int **tab1 = malloc(n * sizeof(int *));
+    int **tab2 = malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        tab1[i] = malloc(m * sizeof(int));
+        tab2[i] = malloc(m * sizeof(int));
+    }
+
+    tab2[0][0] = 10; tab2[0][1] = 20; tab2[0][2] = 30;
+    tab2[1][0] = 40; tab2[1][1] = 50; tab2[1][2] = 60;
+
+    copyArr2D(n, m, tab1, tab2);
+
+    printTable(n, m, tab1);
+
+    for (int i = 0; i < n; i++) {
+        free(tab1[i]);
+        free(tab2[i]);
+    }
+    free(tab1);
+    free(tab2);
+
     return 0;
 }
