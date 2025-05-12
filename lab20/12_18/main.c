75bf6433954fe778bfefe58e2021602a3cc30969
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-12 13:52:37 +0200

    LAB

--- lab20/12_18/main.c
+++ lab20/12_18/main.c
@@ -1,37 +1,35 @@
-/* do poprawy Napisz funkcjê flipDiag, której argumentami s¹ dwie dodatnie liczby ca³kowite n i m oraz dwuwymiarowa tablica elementów (zawieraj¹ca zmienne typu int) wymiaru n x m. Funkcja ma zamieniæ kolejnoœæ elementów le¿¹cych na g³ównej przek¹tnej. Funkcja powinna dzia³aæ tylko dla macierzy kwadratowych, w przeciwnym przypadku nie powinna nic robiæ. Stwórz przypadek testowy.*/
+/* Napisz funkcjê flipDiag, której argumentami s¹ dwie dodatnie liczby ca³kowite n i m oraz dwuwymiarowa tablica elementów (zawieraj¹ca zmienne typu int) wymiaru n x m. Funkcja ma zamieniæ kolejnoœæ elementów le¿¹cych na g³ównej przek¹tnej. Funkcja powinna dzia³aæ tylko dla macierzy kwadratowych, w przeciwnym przypadku nie powinna nic robiæ. Stwórz przypadek testowy.*/
 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void flilDiag(int n, int m, int **tab[]){
-    if(n != m) return;
+void flipDiag(int n, int m, int **tab) {
+    if (n != m) return;
 
-    for (int i = 0; i < n/2; i++){
+    for (int i = 0; i < n / 2; i++) {
         int temp = tab[i][i];
-        tab[i][i] = tab[n - 1 -i];
-        tab[n -1 - i][n - 1 - i] = temp;
+        tab[i][i] = tab[n - 1 - i][n - 1 - i];
+        tab[n - 1 - i][n - 1 - i] = temp;
     }
 }
-
-void printTable(int n, int m, int *tab){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+void printTable(int n, int m, int **tab) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             printf("%4d", tab[i][j]);
         }
         printf("\n");
     }
     printf("-----\n");
 }
-int main()
-{
+
+int main() {
     int n = 3, m = 3;
 
-    int **tab = malloc(n * sizeof(*int));
-    for(int i = 0; i < n; i++){
-        tab[i] = malloc(m * sizeof(int))
+    int **tab = malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        tab[i] = malloc(m * sizeof(int));
     }
-
     tab[0][0] =  2; tab[0][1] =  3; tab[0][2] = -3;
     tab[1][0] =  1; tab[1][1] =  4; tab[1][2] =  7;
     tab[2][0] = -3; tab[2][1] = -6; tab[2][2] = 11;
@@ -44,9 +42,11 @@ int main()
     printf("Po:\n");
     printTable(n, m, tab);
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         free(tab[i]);
     }
     free(tab);
+
     return 0;
 }
+
