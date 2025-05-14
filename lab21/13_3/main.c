b0fcb6d6a80387b76671007220b9a6b7c33e383a
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-14 14:40:48 +0200

    lab

--- lab21/13_3/main.c
+++ lab21/13_3/main.c
@@ -1,16 +1,21 @@
-/* Zaplanuj strukturê Rectangle, maj¹c¹ pola: width (liczba zmiennoprzecinkowa), height (liczba zmiennoprzecinkowa) i color (³añcuch znaków), która ma s³u¿yæ do przechowywania informacji o wymiarach i kolorze prostok¹ta. W main stwórz 4-elementow¹ tablicê zmiennych typu Rectangle. */
+/* LAB Zaplanuj strukturê Rectangle, maj¹c¹ pola: width (liczba zmiennoprzecinkowa), height (liczba zmiennoprzecinkowa) i color (³añcuch znaków), która ma s³u¿yæ do przechowywania informacji o wymiarach i kolorze prostok¹ta. W main stwórz 4-elementow¹ tablicê zmiennych typu Rectangle. */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+struct Rectangle{
     float width;
     float height;
-    char *color
-} Rectangle;
+    char * color;
+};
 
 int main()
 {
-    printf("Hello world!\n");
+    struct Rectangle tab[] ={
+        {4.4, 5.6, "red"},
+        {4.4, 5.6, "red"},
+        {4.4, 5.6, "red"},
+        {4.4, 5.6, "red"}
+    };
     return 0;
 }
