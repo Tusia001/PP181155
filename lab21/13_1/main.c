5910f2210cb0d80bc16db74e8a85b947b29d9d03
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-14 14:00:23 +0200

    lab

---
+++ lab21/13_1/main.c
@@ -0,0 +1,22 @@
+/*Zdefiniuj strukturê Car z polami: brand (napis), model (tablica znaków o rozmiarze 20), year (liczba ca³kowita) i isAvailable (wartoœæ 0 lub 1), która reprezentuje informacjê o dostêpnoœci samochodu na sprzeda¿. W main stwórz kilka zmiennych w typie Car. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Car {
+    char brand[30];
+    char model[20];
+    int year;
+    int isAvailable;
+
+};
+
+int main()
+{
+    struct Car car1 = {"Mitsubishi", "Colt", 2005, 1};
+    struct Car car2 = {"Mazda", "MX-5", 1992, 0};
+
+    printf("Samochod 1: %s %s, Rok: %d, Dostepny: %s\n",
+           car1.brand, car1.model, car1.year, car1.isAvailable ? "Tak" : "Nie");
+    return 0;
+}
