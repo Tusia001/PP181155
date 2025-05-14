5e7eaca0118ace7bb98f466c6dd6c74614874371
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-14 14:30:17 +0200

    lab

--- lab21/13_2/main.c
+++ lab21/13_2/main.c
@@ -1,19 +1,28 @@
-/*Utwórz strukturê Student, która zawiera: firstName (³añcuch znaków), lastName (³añcuch znaków), id (liczba ca³kowita) oraz gpa (liczba zmiennoprzecinkowa), reprezentuj¹c¹ œredni¹ ocen studenta. W main stwórz trzy zmienne w typie Student. Nastêpnie z tych trzech zmiennych stwórz tablicê. */
+/* LAB Utwórz strukturê Student, która zawiera: firstName (³añcuch znaków), lastName (³añcuch znaków), id (liczba ca³kowita) oraz gpa (liczba zmiennoprzecinkowa), reprezentuj¹c¹ œredni¹ ocen studenta. W main stwórz trzy zmienne w typie Student. Nastêpnie z tych trzech zmiennych stwórz tablicê. */
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Student {
-    char *firstName;
+typedef struct {
+    char * firstName;
+    char * lastName;
     int id;
     float gpa;
-};
+} Student;
 
 int main()
 {
-    struct Student student1 = {"Marta", 181155, 4.20};
-    struct Student student2 = {"Piotr", 181325, 4.80};
-    struct Student student3 = {"Tomasz", 171155, 3.23};
+    Student s1 = {"Marta", "Szczepanska", 45, 4.1};
+    Student s2 = {"Tomasz", "gg", 98, 2.3};
+    Student s3 = {"Adam", "kk", 67, 4.2};
 
+    int tablica[3] = {4,5,6};
+    Student tab[] = {s1,s2,s3};
+
+    for(int i=0;i<3;i++){
+        printf("First and last name:%s %s, id:%d, gpa:%f.\n",
+               tab[i].firstName, tab[i].lastName, tab[i].id, tab[i].gpa);
+    }
     return 0;
 }
--- lab21/13_3/main.c
+++ lab21/13_3/main.c
@@ -1,3 +1,5 @@
+/* Zaplanuj strukturê Rectangle, maj¹c¹ pola: width (liczba zmiennoprzecinkowa), height (liczba zmiennoprzecinkowa) i color (³añcuch znaków), która ma s³u¿yæ do przechowywania informacji o wymiarach i kolorze prostok¹ta. W main stwórz 4-elementow¹ tablicê zmiennych typu Rectangle. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
