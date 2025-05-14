c152f68f68bdab3d80fe0c34e97b87635349ef45
Marta Szczepańska <marta.szczepanska001@gmail.com>
2025-05-14 14:02:08 +0200

    lab

---
+++ lab20/12_29/main.c
@@ -0,0 +1,54 @@
+/*Napisz funkcjê snkPrint, która dostaje jako argumenty dwie dodatnie liczby ca³kowite n i m oraz dwuwymiarow¹ tablicê tablic o elementach typu int o wymiarach n x m. Funkcja ma wyœwietliæ elementy tablicy w jednym wierszu rozdzielone spacj¹ na zasadzie “œlimaka,wê¿a” (spójrz na przyk³ad). Stwórz przypadek testowy dla funkcji. */
+#include <stdio.h>
+#include <stdlib.h>
+
+void snkPrint(int n, int m, int ** tab){
+    int i,j;
+    int top =0, bottom =n-1, left =0, right = m-1;
+    int direction =0;
+    while(left <= right && top <= bottom){
+         if (direction ==0){
+            for(j=left; j<=right;j++){
+                printf("%d ", tab[top][j]);
+            }
+            top++;
+            direction=1;
+         }
+         else if (direction ==1){
+            for(i=top;i<=bottom; i++){
+                printf("%d ", tab[i][right]);
+            }
+            right--;
+            direction =2;
+         }
+         else if (direction ==2){
+            for(j=right;j>=left;j--){
+                printf("%d ", tab[bottom][j]);
+            }
+            bottom--;
+            direction=3;
+         }
+         else if (direction ==3){
+            for(i=bottom;i>=top; i--){
+                printf("%d ", tab[i][left]);
+            }
+            left++;
+            direction =0;
+         }
+    }
+}
+
+int main()
+{
+    int ** tab = malloc(4*sizeof(int*));
+    tab[0] = malloc(3*sizeof(int));
+    tab[1] = malloc(3*sizeof(int));
+    tab[2] = malloc(3*sizeof(int));
+    tab[3] = malloc(3*sizeof(int));
+    tab[0][0] =  1; tab[0][1] =  2; tab[0][2] =  3;
+    tab[1][0] =  0; tab[1][1] = -5; tab[1][2] =  8;
+    tab[2][0] =  9; tab[2][1] =  8; tab[2][2] = -2;
+    tab[3][0] = -3; tab[3][1] =  5; tab[3][2] =  4;
+    snkPrint(4,3,tab);
+    return 0;
+}
